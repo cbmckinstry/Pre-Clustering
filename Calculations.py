7970649eb146222f1c10df6a6c68b64bf12433fd
carsonmckinstry <carson.mckinstry2021@gmail.com>
2024-12-03 03:23:02 -0600

    newest

--- Calculations.py
+++ Calculations.py
@@ -1,69 +1,72 @@
 import itertools
-
 def findrange(vehlist):
-    final=[]
-    for item in vehlist:
-        redo=[]
-        for x in range(item//5+1):
-            redo.append([x,(item-(5*x))//6])
-        final.append(redo)
-    return final
+    """
+    Generate possible configurations for each vehicle in vehlist.
+    Each configuration contains how many 5-person and 6-person groups fit in a vehicle.
+    """
+    return [
+        [[x, (capacity - 5 * x) // 6] for x in range(capacity // 5 + 1) if (capacity - 5 * x) >= 0]
+        for capacity in vehlist
+    ]
 
-def find_all_combinations(ent):
-    return list(itertools.product(*ent))
 
-def valid(vehlist,required):
-    poss=find_all_combinations(findrange(vehlist))
-    for item in poss:
-        sum1=0
-        sum2=0
-        for spec in item:
-            sum1+=spec[0]
-            sum2+=spec[1]
-        if sum1>=required[0] and sum2>=required[1]:
-            return True
-    return False
-def validlist(vehlist,required):
-    poss=find_all_combinations(findrange(vehlist))
-    finposs=[]
-    possout=[]
-    for item in poss:
-        sum1=0
-        sum2=0
-        for spec in item:
-            sum1+=spec[0]
-            sum2+=spec[1]
-            if sum1>=required[0] and sum2>=required[1]:
-                if [sum1,sum2] not in finposs:
-                    finposs.append([sum1,sum2])
-                    possout.append(item)
-    return [finposs,possout]
+def validlist(vehlist, required):
+    """
+    Find all valid combinations of groups (5-person and 6-person) that can fit in the vehicles.
+    """
+    ranges = findrange(vehlist)
+    valid_combinations = []
+    example_configs = []
 
-def needed(vehlist,pers5,pers6):
-    largelist=validlist(vehlist,[0,0])[0]
-    largelist1=[]
-    other=[]
-    examples=validlist(vehlist,[0,0])[1]
-    for elem in range(len(largelist)):
-        largelist1.append([pers5-largelist[elem][0],pers6-largelist[elem][1]])
-    for newelem in range(len(largelist)):
-        if largelist1[newelem][0]<0:
-            largelist1[newelem][0]=0
-        if largelist1[newelem][1]<0:
-            largelist1[newelem][1]=0
-    singlelarge=[]
-    for newer in range(len(largelist1)):
-        singlelarge.append(largelist1[newer][0]+largelist1[newer][1])
-    final=[]
-    currentmin=min(singlelarge)
-    for diff in range(len(singlelarge)):
-        if singlelarge[diff]==currentmin and largelist1[diff] not in final:
-            final.append(largelist1[diff])
-            other.append(examples[diff])
-    return [final,other]
+    for combination in itertools.product(*ranges):
+        sum_5 = sum(c[0] for c in combination)
+        sum_6 = sum(c[1] for c in combination)
 
-def spaces(examplelist,vehlist):
-    final=[]
-    for item in range(len(examplelist)):
-        final.append(vehlist[item]-(5*examplelist[item][0]+6*examplelist[item][1]))
-    return final
+        if sum_5 >= required[0] and sum_6 >= required[1]:
+            if [sum_5, sum_6] not in valid_combinations:
+                valid_combinations.append([sum_5, sum_6])
+                example_configs.append(combination)
+
+    return [valid_combinations, example_configs]
+
+
+def needed(vehlist, pers5, pers6):
+    """
+    Determine how many more 5-person and 6-person groups are required to meet the given need.
+    """
+    valid_data = validlist(vehlist, [0, 0])
+    valid_combinations = valid_data[0]
+    example_configs = valid_data[1]
+
+    # Calculate shortfalls for each valid combination
+    shortfalls = [
+        [
+            max(0, pers5 - valid_comb[0]),  # Shortfall of 5-person groups
+            max(0, pers6 - valid_comb[1])  # Shortfall of 6-person groups
+        ]
+        for valid_comb in valid_combinations
+    ]
+
+    # Find the minimum shortfall
+    min_shortfall = min(sum(shortfall) for shortfall in shortfalls)
+    final_combinations = [
+        (shortfall, example_configs[i])
+        for i, shortfall in enumerate(shortfalls)
+        if sum(shortfall) == min_shortfall
+    ]
+
+    # Extract results
+    results = [comb[0] for comb in final_combinations]
+    examples = [comb[1] for comb in final_combinations]
+
+    return [results, examples]
+
+
+def spaces(examplelist, vehlist):
+    """
+    Calculate the remaining space in each vehicle after assigning groups.
+    """
+    return [
+        capacity - (5 * config[0] + 6 * config[1])
+        for capacity, config in zip(vehlist, examplelist)
+    ]
