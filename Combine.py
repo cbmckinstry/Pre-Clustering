41b1f6dfd81d19a7ab9ed9799908d2edf1a2eebc
carsonmckinstry <carson.mckinstry2021@gmail.com>
2025-01-28 23:24:56 -0600

    Redid combine

--- Combine.py
+++ Combine.py
@@ -1,6 +1,8 @@
-def combine(allocations, space, shortfall, backup_size=5, used=None):
+def combine(allocations, space, shortfall, backup_size=5, used=None, boundlst=None):
     if used is None:
         used = set()
+    if boundlst is None:
+        boundlst=[[0,0],[0,0],[0,0],[0,0]]
     backup=shortfall[0]
     six=shortfall[1]
 
@@ -10,13 +12,15 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
         if space[i]!=0:
             allocations0.append(allocations[i])
             space0.append(space[i])
+    lower=boundlst[0][0]
+    upper=boundlst[0][1]
     six4=six
     used4=used.copy()
     combos4=[]
     backup4=backup
     init=[]
     if backup_size==7:
-        for bound in range(1):
+        for bound in range(lower,upper):
             if backup4==0:
                 break
             used4=used.copy()
@@ -29,7 +33,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
                 for n in range(len(space0)-1,m,-1):
                     if backup4==0:
                         break
-                    if (space0[m]+space0[n]>=7) and (m not in used4) and (n not in used4):
+                    if (space0[m]+space0[n]>=7) and (m not in used4) and (n not in used4) and sum(allocations[m])+sum(allocations[n])<=bound:
                         used4.add(m)
                         used4.add(n)
                         combos4.append([m+1,n+1])
@@ -40,7 +44,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
         combos5=combos4.copy()
         used5=used4.copy()
         init1=init.copy()
-        for bound in range(1):
+        for bound in range(lower,upper):
             if six4==0:
                 break
             used5=used4.copy()
@@ -53,7 +57,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
                 for n in range(len(space0)-1,m,-1):
                     if six4==0:
                         break
-                    if (space0[m]+space0[n]>=6) and (m not in used5) and (n not in used5):
+                    if (space0[m]+space0[n]>=6) and (m not in used5) and (n not in used5) and sum(allocations[m])+sum(allocations[n])<=bound:
                         used5.add(m)
                         used5.add(n)
                         combos5.append([m+1,n+1])
@@ -62,7 +66,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
                         if backup4==0 and six4==0:
                             return combos5,init1
     else:
-        for bound in range(1):
+        for bound in range(lower,upper):
             if six4==0:
                 break
             used4=used.copy()
@@ -75,7 +79,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
                 for n in range(len(space0)-1,m,-1):
                     if six4==0:
                         break
-                    if (space0[m]+space0[n]>=6) and (m not in used4) and (n not in used4):
+                    if (space0[m]+space0[n]>=6) and (m not in used4) and (n not in used4) and sum(allocations[m])+sum(allocations[n])<=bound:
                         used4.add(m)
                         used4.add(n)
                         combos4.append([m+1,n+1])
@@ -86,7 +90,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
         combos5=combos4.copy()
         used5=used4.copy()
         init1=init.copy()
-        for bound in range(1):
+        for bound in range(lower,upper):
             if backup4==0:
                 break
             used5=used4.copy()
@@ -99,7 +103,7 @@ def combine(allocations, space, shortfall, backup_size=5, used=None):
                 for n in range(len(space0)-1,m,-1):
                     if backup4==0:
                         break
-                    if (space0[m]+space0[n]>=5) and (m not in used5) and (n not in used5):
+                    if (space0[m]+space0[n]>=5) and (m not in used5) and (n not in used5) and sum(allocations[m])+sum(allocations[n])<=bound:
                         used5.add(m)
                         used5.add(n)
                         combos5.append([m+1,n+1])
